0d5b88708826b44169720f34e5585651e60b75b4
Zengi27 <homola.jan3@gmail.com>
2021-11-12 23:51:49 +0100

    [ADD] admin pages

--- website/admin.py
+++ website/admin.py
@@ -4,5 +4,20 @@ admin = Blueprint("admin",__name__)
 
 @admin.route("/")
 def adminPage():
-    return "<h1>admin fero</h1>"
+    return render_template("/admin/admin.html")
 
+
+@admin.route("/libraries/")
+def libPage():
+    return render_template("admin/libraries.html")
+
+
+@admin.route("/distributors/")
+def distributorsPage():
+    return render_template("admin/distributors.html")
+
+
+@admin.route("/tags/")
+def tagsPage():
+    return render_template("admin/tags.html")
+    
