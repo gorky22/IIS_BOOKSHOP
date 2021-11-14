b00eb369830c761192b2b29f0dde5fdff53281c1
Zengi27 <homola.jan3@gmail.com>
2021-11-14 23:01:36 +0100

    [FIX] - ajax

--- website/admin.py
+++ website/admin.py
@@ -4,31 +4,24 @@ admin = Blueprint("admin",__name__)
 
 @admin.route("/", methods=["POST", "GET"])
 def adminPage():
-    #users = [{'e-mail' : 'gorky@srac.sk', 'name' : 'Pan gorky', 'data' : '21.12.1988'}]
-
-
     if request.method == "POST":
         if "nm" in request.form:
             users = [{'e-mail' : 'najdeny@email', 'name' : request.form["nm"], 'data' : '21.12.1988'}]
-        else:
-            users = [{'e-mail' : 'gorky@srac.sk', 'name' : 'Pan gorky', 'data' : '21.12.1988'},
-                    {'e-mail' : 'Hanzik@beast.sk', 'name' : 'Janicko', 'data' : '11.10.1982'},
-                    {'e-mail' : 'Tomik@mergesort.sk', 'name' : 'Shelby z brna', 'data' : '1.1.2002'}]
+            
     else:
-        users = [{'e-mail' : 'gorky@srac.sk', 'name' : 'Pan gorky', 'data' : '21.12.1988'}]
+        users = [{'e-mail' : 'gorky@srac.sk', 'name' : 'Pan gorky', 'data' : '21.12.1988'},
+                 {'e-mail' : 'Hanzik@beast.sk', 'name' : 'Janicko', 'data' : '11.10.1982'},
+                 {'e-mail' : 'Tomik@mergesort.sk', 'name' : 'Shelby z brna', 'data' : '1.1.2002'}]
 
     return render_template("/admin/admin.html", users=users)
 
 
-@admin.route("/admin/delete/", methods=["POST"])
+@admin.route("delete/", methods=["POST"])
 def userDelete():
-    fero = {"name": "sprava prijata"}
     
     if request.method == "POST" :
-    
         fero = {"name": "sprava prijata"}
         return fero
-    return fero
 
 @admin.route("/libraries/")
 def libPage():
