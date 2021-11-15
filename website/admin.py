0077542c18392ba900ef7b3eef02990873e4f89b
Zengi27 <homola.jan3@gmail.com>
2021-11-15 13:23:14 +0100

    [ADD] - users from db

--- website/admin.py
+++ website/admin.py
@@ -1,17 +1,21 @@
 from flask import Blueprint, render_template, request
 
+from website.database import delete_user, get_all_users, get_user_with_this_email
+
 admin = Blueprint("admin",__name__)
 
 @admin.route("/", methods=["POST", "GET"])
 def adminPage():
     if request.method == "POST":
         if "nm" in request.form:
-            users = [{'e-mail' : 'najdeny@email', 'name' : request.form["nm"], 'data' : '21.12.1988'}]
+            #users = get_user_with_this_email(request.form["nm"])
+            users = [{'email' : 'najdeny@email', 'name' : request.form["nm"], 'data' : '21.12.1988'}]
             
     else:
-        users = [{'e-mail' : 'gorky@srac.sk', 'name' : 'Pan gorky', 'data' : '21.12.1988'},
-                 {'e-mail' : 'Hanzik@beast.sk', 'name' : 'Janicko', 'data' : '11.10.1982'},
-                 {'e-mail' : 'Tomik@mergesort.sk', 'name' : 'Shelby z brna', 'data' : '1.1.2002'}]
+        users = get_all_users()
+        #users = [{'e-mail' : 'gorky@srac.sk', 'name' : 'Pan gorky', 'data' : '21.12.1988'},
+        #         {'e-mail' : 'Hanzik@beast.sk', 'name' : 'Janicko', 'data' : '11.10.1982'},
+        #         {'e-mail' : 'Tomik@mergesort.sk', 'name' : 'Shelby z brna', 'data' : '1.1.2002'}]
 
     return render_template("/admin/admin.html", users=users)
 
@@ -20,8 +24,10 @@ def adminPage():
 def userDelete():
     
     if request.method == "POST" :
-        fero = {"name": "sprava prijata"}
-        return fero
+        email = request.form.get("email")  
+        #delete_user(email)
+
+        return {'message' : f'Vymazal si uzivatela {email}'}
 
 @admin.route("/libraries/")
 def libPage():
