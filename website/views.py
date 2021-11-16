94647ac3b6ae5a33596ee3ff1ab54e720adabd88
icyfirecz <tkrivanek2@gmail.com>
2021-11-16 14:23:27 +0100

    [add]

--- website/views.py
+++ website/views.py
@@ -4,34 +4,55 @@ import random
 
 views = Blueprint("views",__name__)
 genres = db_genres()
+
+def format_book_and_authors(book):
+    new_book = {'title_name':book[0]['title_name'],
+        'rating':book[0]['rating'],
+        'path_to_picture':book[0]['path_to_picture'],
+        'authors':[]
+    }
+
+    for book_author in book:
+        author = " ".join([book_author['author_name'], book_author['author_surname']])
+        new_book['authors'].append(author)
+
+    new_book['rating_path'] = '/static/img/rating/' + str(round(new_book['rating'])*10)+'percent.png'
+    return new_book
+
+def group_by_five(books):
+    result = []
+    for i in range(0,len(books),5):
+        end = i+5 if (i+5) < len(books) else len(books)
+        tmp = []
+        for book in books[i:end]:
+            tmp.append(book)
+        result.append(tmp)
+    return result
+
+def format_ratings(books):
+    for book in books:
+        book['rating_path'] = '/static/img/rating/'+str(round(book['rating'])*10)+'percent.png'
+
+
 @views.route("/")
 def viewsPage():
     top = db_top_books()
     
     randomlist = random.sample(range(0, 9), 5)
     books = [top[i] for i in randomlist]
-    info = [db_book_info(book['title_id']) for book in books]
-    print(*info,sep='\n')
-    for book in books:
-        book['rating_path'] = '/static/img/rating/' + str(round(book['rating'])*10)+'percent.png'
-
+    books = [db_book_info(book['title_id']) for book in books]
+    books = [format_book_and_authors(book) for book in books]
     
     return render_template("/main/main.html",books=books,genres=genres)
 
 @views.route("/list/")
 def listPage():
-    books = db_books()
-    
-    result = []
-    for i in range(0,len(books),5):
-        end = i+5 if (i+5) < len(books) else len(books)
-        tmp = []
-        for book in books[i:end]:
-            book['rating_path'] = '/static/img/rating/' + str(round(book['rating'])*10)+'percent.png'
-            tmp.append(book)
-        result.append(tmp)
+    books = db_all_book_info()
+    format_ratings(books)
+    books = group_by_five(books)
+    print(books)
 
-    return render_template("/main/list.html",books=result,genres=genres)
+    return render_template("/main/list.html",books=books,genres=genres)
     
 @views.route("/detail/")
 def detailPage():
@@ -85,8 +106,3 @@ def bookDetail(bookid):
     return render_template('/main/detail.html',book=book,genres=genres)
 
 
-#[{'title_name': 'Sila geografie v 21. storočí',
-# 'author_name': 'Johannes',
-# 'author_surname': 'Straus'},
-# {'title_name': 'Sila geografie v 21. storočí',
-# 'author_name': 'Terry', 'author_surname': 'Wilson'}]
