1fe85acb91850741525afd31d140c52b2f876162
icyfirecz <tkrivanek2@gmail.com>
2021-11-18 22:55:39 +0100

    back from heroku

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from website import create_app, db_connection
 
-
-app = create_app()
-    
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
+    db_connection.close()
