f818a4a9674b8462c8dfb9b165e637ad4f33b89a
icyfirecz <tkrivanek2@gmail.com>
2021-11-19 14:30:17 +0100

    [HEROKU]

--- app.py
+++ app.py
@@ -2,7 +2,5 @@
 # -*- coding: utf-8 -*-
 from website import create_app, db_connection
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
-    db_connection.close()
+
+app = create_app()
